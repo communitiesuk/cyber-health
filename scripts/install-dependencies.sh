d69b939e1911c74ba58d99b9d824be5987589e3d
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-12 23:20:52 +0000

    apt-geet update?

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y firefox-geckodriver
 gem install dpl --pre
 npm install npm@latest -g
 npm install http-server -g
-apt-get install -y firefox-geckodriver
 cd accessibility || exit
 npm install
 which firefox
