4086c529a4ef3556ef45db912fac8367d172b801
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-12 21:33:16 +0000

    Chrome options

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,5 +3,5 @@
 gem install dpl --pre
 npm install http-server -g
 cd accessibility || exit
-npm install chromedriver --chromedriver_skip_download=true
+npm install chromedriver --chromedriver_skip_download=true  
 npm install
