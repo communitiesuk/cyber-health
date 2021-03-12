e88a1fb29a9943c851f42226ee2e03aca47bf4a3
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-12 16:11:12 +0000

    debugging the present working directory

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -2,9 +2,12 @@
 
 APPLICATIONS=("frontend")
 
+pwd
+
 for application in "${APPLICATIONS[@]}"
 do
     (
+        echo "Running in the folder $application"
         cd "$application" || exit
         # shellcheck disable=SC1091
         source scripts/start.sh 
