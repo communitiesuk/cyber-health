ad345adb49cabb167c12377d68904ce140dcdf5d
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-13 20:43:35 +0000

    Fix to name of unzipped path

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -7,7 +7,7 @@ npm install npm@latest -g
 npm install http-server -g
 wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip
 unzip dependency-check-6.1.2-release.zip
-chmod 755 dependency-check-6.1.2-release/bin/*.sh
+chmod 755 dependency-check/bin/*.sh
 cd accessibility || exit
 npm install
 which firefox
