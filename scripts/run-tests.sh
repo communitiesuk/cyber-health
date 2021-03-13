42c14aeb575533ad4268c866ee4f7d35a9f333c1
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-13 22:07:39 +0000

    change the way that tests are called

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -8,6 +8,9 @@ for application in "${APPLICATIONS[@]}"
 do
     (
         echo "runnning dependency check on ./$application"
-        dependency-check/bin/dependency-check.sh --project "$application" --scan "./$application" -f JSON --failOnCVSS 5
+        dependency-check/bin/dependency-check.sh --project "$application" --scan "./$application" -f JSON 
+        # --failOnCVSS 5
+        echo "Dependency Report"
+        cat dependency-check-report.json
     )
 done
