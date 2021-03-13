b5c6271f3cde273a49931b906ced4b6d30861480
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-13 22:16:08 +0000

    fix the scripts path

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,6 @@
 # Run accessibility tests
 node accessibility/index.js
 
-# Run Static Analysis with Dependency Check 
+# Run Static Analysis with Dependency Check
 # shellcheck disable=SC1091
-source dependency-check.sh
+source scripts/dependency-check.sh
