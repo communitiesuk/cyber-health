438db88f2186507bc2476fb4e27e8bff07b1629c
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-22 12:09:18 +0000

    Seperate items into different lines

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Create the database users expected along with databases
-sudo -u postgres psql postgres -c "CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD' CREATEDB; CREATE DATABASE $DATABASE_NAME OWNER $DATABASE_USER;"
+sudo -u postgres psql postgres -c "CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD' CREATEDB;"
+sudo -u postgres psql postgres -c "CREATE DATABASE $DATABASE_NAME OWNER $DATABASE_USER;"
 
 echo "Confirm database is running: pg_isready"
 pg_isready -d "$DATABASE_NAME" -U "$DATABASE_USER" -t 5
