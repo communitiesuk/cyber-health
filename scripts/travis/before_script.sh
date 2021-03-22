71e8ee305fa5919782a98bd082b5e5f5c04e56e2
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-22 11:03:57 +0000

    Tweaks to try and support postgres

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,10 +2,9 @@
 
 # Create the database users expected
 psql -c "create database $DATABASE_NAME;" -U postgres
-psql -c "CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD'" -U postgres ;
 
 echo "Confirm database is running: pg_isready"
-pg_isready -d "$DATABASE_NAME" U "DATABASE_USER" -t 5
+pg_isready -d "$DATABASE_NAME" -U "$DATABASE_USER" -t 5
 
 APPLICATIONS=("cyber-health")
 for application in "${APPLICATIONS[@]}"
