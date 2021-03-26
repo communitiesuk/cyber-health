f4f887a462daab76fec36b06bc866c010057d84b
Home PC <h0usemaintenance@hotmail.com>
2021-03-26 18:48:27 +0000

    Making changes to deployment process

--- cyber_health/cyber_health/settings.py
+++ cyber_health/cyber_health/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
