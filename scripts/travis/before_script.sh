ac39a39b530caa60b0e2afd01471915e4dfda7e2
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-30 09:02:42 +0100

    Add fail on errors to the project

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -10,6 +10,16 @@ do
     (
         echo "Running in the folder $application"
         cd "$application" || exit
+
+        # Running safety check and bandit if the folder contains a requirements.txt
+        if test -f "requirements.txt"; then
+           safety check -r requirements.txt  --json
+           bandit -r . -f json
+        fi
+
+        # Running the unit tests
+        python manage.py test
+
         # shellcheck disable=SC1091
         source scripts/start.sh 
     )
