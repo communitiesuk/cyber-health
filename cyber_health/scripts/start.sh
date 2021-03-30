32bedac25795750abd63f6ff66332fa02089f5e1
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-30 08:59:16 +0100

    Updates to the startup script for travis

--- cyber_health/scripts/start.sh
+++ cyber_health/scripts/start.sh
@@ -6,4 +6,17 @@ DEFAULT_FRONTEND_PORT="8080"
 # Script cloud foundry
 FRONTEND_PORT="${FRONTEND_PORT:=$DEFAULT_FRONTEND_PORT}"
 
-manage.py runserver $DEFAULT_FRONTEND_PORT
+# Setup the virtual environment
+python3 -m venv cyber-health-python
+# shellcheck disable=SC1091
+source ./cyber-health-python/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# NPM install dependencies
+npm install
+npm run frontend:build
+
+# Start the application
+python manage.py runserver $DEFAULT_FRONTEND_PORT
