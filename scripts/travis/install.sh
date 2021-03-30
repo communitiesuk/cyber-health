97f45e2d6514d9df30f69495d4955e4345942faf
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-30 11:42:43 +0100

    I need to seperate out the setup from the sourcing

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -5,7 +5,7 @@ export LC_CTYPE="en_GB.UTF-8"
 sudo dpkg-reconfigure locales
 
 sudo apt-get update
-sudo apt-get install -y firefox-geckodriver wget unzip python3.9-venv python3.8-venv
+sudo apt-get install -y firefox-geckodriver wget unzip python3.9-venv python3.8-venv 
 sudo -H pip install safety
 sudo -H pip install bandit
 sudo -H pip install wheel
