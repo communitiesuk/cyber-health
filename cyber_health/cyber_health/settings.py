8838ce0366de3bdde7f8ce02a87465fac7c0850c
Home PC <h0usemaintenance@hotmail.com>
2021-03-30 15:19:10 +0100

    Making amendments to the pipeline

--- cyber_health/cyber_health/settings.py
+++ cyber_health/cyber_health/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.london.cloudapps.digital', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cyber_health-sandbox.london.cloudapps.digital', ]
 
 # Application definition
 
