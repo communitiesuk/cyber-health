a9d8920b772d219351f1b65f7ddbb5c3b85a2572
Joel Stobart <joel.stobart@wearesnook.com>
2021-03-30 15:07:07 +0100

    Move wheel

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -8,7 +8,6 @@ sudo apt-get update
 sudo apt-get install -y firefox-geckodriver wget unzip python3.9-venv python3.8-venv 
 sudo -H pip3 install safety
 sudo -H pip3 install bandit
-sudo -H pip3 install wheel
 
 
 gem install dpl --pre
