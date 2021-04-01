f5512576872d558666d3398119d922b86404c945
Home PC <h0usemaintenance@hotmail.com>
2021-04-01 13:16:14 +0100

    Cleaning up develop branch

--- CyberHealth/CyberHealth/settings.py
+++ CyberHealth/CyberHealth/settings.py
@@ -27,9 +27,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cyberhealth-sandbox.london.cloudapps.digital']
 
 
 # Application definition
