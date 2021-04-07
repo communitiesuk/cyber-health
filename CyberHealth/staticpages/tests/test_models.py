0afc3455c555acd82a05a4b474bfdb93bb1ef14d
kingdomlevel <nllmrrs@aol.com>
2021-04-07 13:55:53 +0100

    creates skeleton model test files (currently empty)

---
+++ CyberHealth/staticpages/tests/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from staticpages.models import *
+
+# tests for each model used by the staticpages app will go here
+
+class SomeModelTestCase(TestCase):
+  pass
+  # def setUp(self):
+    # self.some_object = SomeModel.objects.create()
+
+  # def test_object_has_name(self):
+  #   self.assertEqual("Section 1: MANAGE security risk", self.some_object.name)
