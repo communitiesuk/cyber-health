a9fa8c3f55662dfcbe1c6b11dc02ab09c76adc2a
Home PC <h0usemaintenance@hotmail.com>
2021-04-08 17:41:33 +0100

    Amending tests and script

--- CyberHealth/scripts/setup.sh
+++ CyberHealth/scripts/setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+echo "*************************************************************************************************************"
+# Setup the virtual environment
+python3 -m venv cyber-health-python
 
+echo "Activating ./cyber-health-python/bin/activate"
+# shellcheck disable=SC1091
+source ./cyber-health-python/bin/activate
+echo "*************************************************************************************************************"
 # Ensure that we have the prerequisites
 python3 -m pip install -U pipenv
+python3 -m pip install -U pip
 python3 -m pip install -U setuptools
 pip3 install wheel
 pip3 install whitenoise
-
+echo "*************************************************************************************************************"
 # Install dependencies
-pipenv shell
-pipenv install -r requirements.txt
-
+pip3 install -r requirements.txt
+pipenv install
+echo "*************************************************************************************************************"
 # NPM install dependencies
 npm install
 npm run frontend:build
+echo "*************************************************************************************************************"
