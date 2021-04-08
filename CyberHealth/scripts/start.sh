4c2e7fde43de256e3624fc8fd23eca40635a5640
Angie <h0usemaintenance@hotmail.com>
2021-04-08 23:09:02 +0100

    Resolving missing django module

--- CyberHealth/scripts/start.sh
+++ CyberHealth/scripts/start.sh
@@ -9,6 +9,6 @@ FRONTEND_PORT="${FRONTEND_PORT:=$DEFAULT_FRONTEND_PORT}"
 ## shellcheck disable=SC1091
 #source ./cyber-health-python/bin/activate
 
-# Start the application
-echo "Starting the webserver using the command 'python3 manage.py runserver $FRONTEND_PORT'"
-python3 manage.py runserver $FRONTEND_PORT &
+## Start the application
+#echo "Starting the webserver using the command 'python3 manage.py runserver $FRONTEND_PORT'"
+#python3 manage.py runserver $FRONTEND_PORT &
