5c35e198c2124a4a5f1da8aec3e0cac1949c5aa6
Home PC <h0usemaintenance@hotmail.com>
2021-04-09 15:28:31 +0100

    Amending scripts

--- CyberHealth/CyberHealth/urls.py
+++ CyberHealth/CyberHealth/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dynamic/', include('dynamicpages.urls')),
     path('', include('staticpages.urls')),
-
 ]
