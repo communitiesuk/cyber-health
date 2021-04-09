f9a11910d477c887e9d13b8f50cea6d7ac35723b
Home PC <h0usemaintenance@hotmail.com>
2021-04-09 12:36:25 +0100

    Amending scripts

--- CyberHealth/CyberHealth/settings.py
+++ CyberHealth/CyberHealth/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 STATICFILES_DIRS = [
     os.path.join(STATIC_URL, 'dist'), 
 ]
