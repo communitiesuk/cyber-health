9d03bf669376ae0ce26197cae27875fbd50541bc
Home PC <h0usemaintenance@hotmail.com>
2021-04-09 09:57:01 +0100

    Amending scripts

--- scripts/dependency-check.sh
+++ scripts/dependency-check.sh
@@ -5,7 +5,7 @@ APPLICATIONS=("CyberHealth")
 for application in "${APPLICATIONS[@]}"
 do
     (
-        echo "running dependency check on ./$application"
+        echo "runnning dependency check on ./$application"
         dependency-check/bin/dependency-check.sh --failOnCVSS 8 --disableAssembly --project "$application" --scan "./$application" -f JSON 
     )
 done
