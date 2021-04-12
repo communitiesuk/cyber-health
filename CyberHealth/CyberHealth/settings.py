8b552e101e8e330f9bb41909c28e59635e296c7b
Adam Parsons <adam@adamparsons.me>
2021-04-12 14:16:48 +0100

    changed static root dir

--- CyberHealth/CyberHealth/settings.py
+++ CyberHealth/CyberHealth/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATIC_ROOT = os.path.join(STATIC_URL, 'root')
 STATICFILES_DIRS = [
     os.path.join(STATIC_URL, 'dist'), 
 ]
