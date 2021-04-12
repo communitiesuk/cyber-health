fe5c3fe02291edf458ae0652ff0a5ffaa60b508a
kingdomlevel <nllmrrs@aol.com>
2021-04-12 11:21:40 +0100

    registers urls for assessment app in CyberHealth app and renders sample question data to sample index page

--- CyberHealth/CyberHealth/urls.py
+++ CyberHealth/CyberHealth/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('assessment/', include('assessment.urls')),
     path('dynamic/', include('dynamicpages.urls')),
     path('', include('staticpages.urls')),
 ]
