6eac428b5d9c488e0c89c5faa23f8e695d57ea85
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-13 16:24:08 +0100

    Make the tests fail more agressively in this context

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -11,12 +11,6 @@ do
         echo "Running in the folder $application"
         cd "$application" || exit
 
-        # Running safety check and bandit if the folder contains a requirements.txt
-        if test -f "requirements.txt"; then
-           safety check -r requirements.txt  --json
-           bandit -r . -f json
-        fi
-
         export SECRET_KEY="travis_secret_key_1"
 
         source scripts/setup.sh
@@ -24,8 +18,6 @@ do
         # shellcheck disable=SC1091
         source ./cyber-health-python/bin/activate
 
-        # Running the unit tests
-        python3 manage.py test
 
         # shellcheck disable=SC1091
         source scripts/start.sh 
