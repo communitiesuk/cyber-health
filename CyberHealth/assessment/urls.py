56c0f38562d11d471d3b675f0b706df99dd842cb
Adam Parsons <adam@adamparsons.me>
2021-04-13 11:19:15 +0100

    added question routes and views to show question content on page

--- CyberHealth/assessment/urls.py
+++ CyberHealth/assessment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name="index"),
+  path('question/<int:question_id>', views.question, name="question"),
 ]
--- CyberHealth/assessment/views.py
+++ CyberHealth/assessment/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from assessment.models import Question
 
 # Create your views here.
@@ -8,3 +9,14 @@ def index(request):
       question_text="Are users who install software or other active code on the Council’s systems without permission subject to disciplinary action?"
   )
   return render(request, 'assessment/index.html', {'question': test_q})
+
+
+def question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {
+        'question' : question
+    }
+
+
+    return render(request, 'assessment/question.html', context)
