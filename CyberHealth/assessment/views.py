4106e6fba0de8de00e57404f7b959b1274812f93
kingdomlevel <nllmrrs@aol.com>
2021-04-13 16:02:07 +0100

    adds index page from dynamic pages app

--- CyberHealth/assessment/views.py
+++ CyberHealth/assessment/views.py
@@ -4,7 +4,4 @@ from assessment.models import Question
 # Create your views here.
 
 def index(request):
-  test_q = Question.objects.create(
-      question_text="Are users who install software or other active code on the Council’s systems without permission subject to disciplinary action?"
-  )
-  return render(request, 'assessment/index.html', {'question': test_q})
+  return render(request, 'assessment/index.html')
