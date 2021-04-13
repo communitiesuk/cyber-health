ef4d53adb50eaa056819f9ea26d6256aaf2a0e60
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-13 20:49:01 +0100

    Fix a configuration issue and remove stray additions

--- scripts/setup-postgres.sh
+++ scripts/setup-postgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the database users expected along with databases
-sudo -u postgres psql postgres -c "CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD' CREATEDB;"
+sudo -u postgres psql postgres -c "CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASS' CREATEDB;"
 sudo -u postgres psql postgres -c "CREATE DATABASE $DATABASE_NAME OWNER $DATABASE_USER;"
 sudo -u postgres psql postgres -c "ALTER ROLE $DATABASE_USER SET client_encoding TO 'utf8';"
 sudo -u postgres psql postgres -c "ALTER ROLE $DATABASE_USER SET default_transaction_isolation TO 'read committed';"
@@ -11,4 +11,4 @@ sudo -u postgres psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NA
 echo "Confirm database is running: pg_isready"
 pg_isready -d "$DATABASE_NAME" -U "$DATABASE_USER" -t 5
 echo "url:$DATABASE_URL" 
-echo "pwd:$DATABASE_PASSWORD"
+echo "pwd:$DATABASE_PASS"
