7bb17d48f70b9a0617f7df292a51225f6c0e257c
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-14 15:02:25 +0100

    Changing the dependency check version to use an environment variable

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -9,8 +9,8 @@
 gem install dpl --pre
 npm install npm@latest -g
 npm install http-server -g
-wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.5/dependency-check-6.1.5-release.zip
-unzip dependency-check-6.1.4-release.zip
+wget "https://github.com/jeremylong/DependencyCheck/releases/download/v$DEPENDENCY_CHECK_VERSION/dependency-check-$DEPENDENCY_CHECK_VERSION-release.zip"
+unzip "dependency-check-$DEPENDENCY_CHECK_VERSION-release.zip"
 chmod 755 dependency-check/bin/*.sh
 
 TESTS=("accessibility" "acceptance")
