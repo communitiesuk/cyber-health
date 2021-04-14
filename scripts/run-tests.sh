592954ca5e91984620bbc4bdffb0814d1279b1a9
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-14 12:16:34 +0100

    Try to improve test feedback

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
 APPLICATIONS=("CyberHealth")
 for application in "${APPLICATIONS[@]}"
 do
@@ -9,18 +13,25 @@ do
         echo "Checking if the codebase is Python (has requirements.txt)"
         # Running safety check and bandit if the folder contains a requirements.txt
         if test -f "requirements.txt"; then
-            echo "It is - So running bandit and safety on the codebases"
+            echo "It is - So running bandit, safety and unit tests on the codebases"
 
             # shellcheck disable=SC1091
             source ./cyber-health-python/bin/activate
             
-            safety check -r requirements.txt  --json
-            
-            bandit -r . -f json
-
-            echo "Running unit tests"
-            python3 manage.py test
-
+            if safety check -r requirements.txt  --json; then
+                echo "${RED}Failed:${NC} Safety Check for $application"
+            else 
+                 if ! bandit -r . -f json; then
+                    echo "${RED}Failed:${NC} Bandit for $application"
+                else 
+                    echo "${RED}Running unit tests"
+                    if ! python3 manage.py test; then
+                        echo "${RED}Failed:${NC} unit tests for $application"
+                    else 
+                        echo "${GREEN}Passed:${NC} All direct tests for $application passed"
+                    fi
+                fi    
+            fi
         fi
 
     )
@@ -32,6 +43,8 @@ do
     (
         echo "Running the tests $test"
         cd "$test" || exit 
-        npm run test
+        if ! npm run test ; then
+            echo "${RED}Failed:${NC} $test for $application"
+        fi 
     )
 done
