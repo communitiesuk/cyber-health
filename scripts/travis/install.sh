e6d823a48063804ef366d598bed19a2fb1ccb577
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-15 11:03:23 +0100

    Remove the export lines

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# export LC_ALL="en_GB.UTF-8"
-# export LC_CTYPE="en_GB.UTF-8"
-# sudo dpkg-reconfigure locales
-
-
-
 gem install dpl --pre
 npm install npm@latest -g
 npm install http-server -g
