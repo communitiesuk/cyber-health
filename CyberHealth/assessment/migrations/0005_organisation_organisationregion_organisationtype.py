997d702d473e0ad9cc45df69f68507f557be9675
Joel Stobart <joel.stobart@wearesnook.com>
2021-04-30 16:57:51 +0100

    Work in progress

--- CyberHealth/assessment/migrations/0005_organisation_organisationregion_organisationtype.py
+++ CyberHealth/assessment/migrations/0005_organisation_organisationregion_organisationtype.py
@@ -31,8 +31,10 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('domain_name', models.CharField(max_length=253)),
-                ('organisation_region', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='assessment.organisationregion')),
-                ('organisation_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='assessment.organisationtype')),
+                ('organisation_region', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                                          to='assessment.organisationregion')),
+                ('organisation_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                                        to='assessment.organisationtype')),
             ],
         ),
     ]
