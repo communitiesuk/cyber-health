8782438fb8b6430fedb692777b7fd8ad575b874a
kingdomlevel <nllmrrs@aol.com>
2021-05-05 11:13:51 +0100

    #LGCS-635 adds URL, view + template with placeholder content for privacy policy

--- CyberHealth/staticpages/urls.py
+++ CyberHealth/staticpages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.start_page, name='static-page'),
+    path('privacy-policy', views.privacy_policy, name="privacy-policy"),
 ]
--- CyberHealth/staticpages/views.py
+++ CyberHealth/staticpages/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 @basic_auth_required
 def start_page(request):
     return render(request, 'staticpages/index.html')
+
+def privacy_policy(request):
+    return render(request, 'staticpages/privacy-policy.html')
