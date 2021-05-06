3cf6c24907e5db5c33004e2a6a43f1b1b36bc54f
kingdomlevel <nllmrrs@aol.com>
2021-05-06 18:31:27 +0100

    adds test to check cookie policy link renders in footer

--- acceptance/features/footer.feature
+++ acceptance/features/footer.feature
@@ -4,3 +4,8 @@ Scenario: Footer contains link to privacy policy
 Given I am a Cyber Capable Person
 When I visit the Cyber Health Framework site
 Then I see a link to "/privacy-policy" in the footer
+
+Scenario: Footer contains link to cookie policy
+Given I am a Cyber Capable Person
+When I visit the Cyber Health Framework site
+Then I see a link to "/cookie-policy" in the footer
--- acceptance/steps/footer.test.js
+++ acceptance/steps/footer.test.js
@@ -37,4 +37,32 @@ JestCucumber.defineFeature(feature, test => {
       driver.quit();
     });
   });
+
+  test('Footer contains link to cookie policy', ({ given, when, then }) => {
+
+    let url = "";
+    let driver;
+
+    given('I am a Cyber Capable Person', () => {
+      driver = new WebDriver.Builder()
+        .withCapabilities(WebDriver.Capabilities.firefox())
+        .setFirefoxOptions(new firefox.Options()
+          .headless()
+          .windowSize(screen)
+        )
+        .build();
+    });
+
+    when('I visit the Cyber Health Framework site', async () => {
+      url = `${process.env.FRONTEND_PROTO}://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`
+      await driver.get(url).catch(urlCaptureException => { console.error(urlCaptureException) })
+    });
+
+    then(/I see a link to \"(.*)\" in the footer/, async (expected) => {
+      const footerElement = await driver.findElement(WebDriver.By.css(`footer a[href="${expected}"]`));
+      const actualFooterElement = await footerElement.getText();
+      expect(actualFooterElement).toEqual('Cookies');
+      driver.quit();
+    });
+  });
 });
