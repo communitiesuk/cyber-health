3d27951efe7af713310682d1f274f2ca66c2fcc8
kingdomlevel <nllmrrs@aol.com>
2021-05-06 12:25:57 +0100

    #LGCS-638 first acceptance test for cookie policy page

---
+++ acceptance/steps/cookie-policy.test.js
@@ -0,0 +1,41 @@
+const JestCucumber = require('jest-cucumber')
+const WebDriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+
+const screen = {
+  width: 1024,
+  height: 768
+};
+
+const feature = JestCucumber.loadFeature('features/cookie-policy.feature');
+
+JestCucumber.defineFeature(feature, test => {
+  test('Cookie policy page contains expected heading', ({ given, when, then }) => {
+
+    let url = "";
+    let driver;
+
+    given('I am a Cyber Capable Person', () => {
+      driver = new WebDriver.Builder()
+        .withCapabilities(WebDriver.Capabilities.firefox())
+        .setFirefoxOptions(new firefox.Options()
+          .headless()
+          .windowSize(screen)
+        )
+        .build();
+    });
+
+    when('I visit the Cyber Health Framework site Cookie policy page', async () => {
+      const urlStub = 'cookie-policy'
+      url = `${process.env.FRONTEND_PROTO}://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}/${urlStub}`
+      await driver.get(url).catch(urlCaptureException => { console.error(urlCaptureException) })
+    });
+
+    then(/I see a page with the heading \"(.*)\"/, async (expected) => {
+      const pageTitle = await driver.findElement(WebDriver.By.id('main-content'))
+      const actual = await pageTitle.getText()
+      expect(actual).toContain(expected)
+      driver.quit();
+    });
+  });
+});
