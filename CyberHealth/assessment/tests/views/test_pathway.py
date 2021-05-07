1b6931d2ed98c26b3584fe1088b1f9df912d90fd
Joel Stobart <joel.stobart@wearesnook.com>
2021-05-07 08:59:40 +0100

    Add in the tests

---
+++ CyberHealth/assessment/tests/views/test_pathway.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.urls import reverse
+from assessment.models import Pathway, PathwayGroup
+
+
+class PathwayViewTest(TestCase):
+
+    def setUp(self):
+        self.heading = "Hello"
+        self.description = "Welcome to your overview page. Use this page to select which parts of the assessment " \
+                           "you’d like to complete and to view your progress. "
+
+        self.pathway_group = PathwayGroup.objects.create(
+            name="Pathway group 1",
+            intro_text="This is pathway group 1",
+            slug="pathway-group-1"
+        )
+
+        self.pathway1 = Pathway.objects.create(
+            long_name="National Sector Network",
+            short_name="NSN",
+            intro_text="This is a description for the NSN",
+            pathway_group=self.pathway_group
+        )
+
+    def test_pathway_view_url_response_ok(self):
+        response = self.client.get('/assessment/nsn')
+        self.assertEqual(200, response.status_code)
+
+    def test_pathway_page_uses_intended_template(self):
+        response = self.client.get('/assessment/nsn')
+        self.assertTemplateUsed(
+            response, 'assessment/pathway.html', 'base.html')
+
+    def test_pathway_page_displays_heading(self):
+        response = self.client.get('/assessment/nsn')
+        self.assertContains(
+            response, "National Sector Network")
+
+    def test_pathway_page_displays_intro_text(self):
+        response = self.client.get('/assessment/nsn')
+        self.assertContains(
+            response, "This is a description for the NSN")
