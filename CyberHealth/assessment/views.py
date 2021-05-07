75e795aa5b8d0e699b8c63c8029eaf07bfbe0742
h0usemaintenance <h0usemaintenance@hotmail.com>
2021-05-07 13:22:22 +0100

    Removing redundant code

--- CyberHealth/assessment/views.py
+++ CyberHealth/assessment/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from assessment.models import Question, Answer, Choice
 from basicauth.decorators import basic_auth_required
