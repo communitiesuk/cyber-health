b378d2394374adfbf2f89a341802c64c17057275
kingdomlevel <nllmrrs@aol.com>
2021-05-07 16:18:27 +0100

    exemplifies use of webdriver wrapper class for acceptance testing in accessiblity statement test

--- acceptance/steps/accessibility-statement.test.js
+++ acceptance/steps/accessibility-statement.test.js
@@ -1,38 +1,22 @@
 const JestCucumber = require('jest-cucumber')
-const WebDriver = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-
-const screen = {
-  width: 1024,
-  height: 768
-};
+const FirefoxDriver = require('../helpers/FirefoxDriver.js');
 
 const feature = JestCucumber.loadFeature('features/accessibility-statement.feature');
 
 JestCucumber.defineFeature(feature, test => {
   test('Accessibility statement page contains expected heading', ({ given, when, then }) => {
-
-    let url = "";
     let driver;
 
     given('I am a Cyber Capable Person interested in accessibility', () => {
-      driver = new WebDriver.Builder()
-        .withCapabilities(WebDriver.Capabilities.firefox())
-        .setFirefoxOptions(new firefox.Options()
-          .headless()
-          .windowSize(screen)
-        )
-        .build();
+      driver = new FirefoxDriver();
     });
 
     when('I visit the Cyber Health Framework site Accessibility statement page', async () => {
-      const urlStub = 'accessibility-statement'
-      url = `${process.env.FRONTEND_PROTO}://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}/${urlStub}`
-      await driver.get(url).catch(urlCaptureException => { console.error(urlCaptureException) })
+      await driver.visitPage('accessibility-statement');
     });
 
     then(/I see a page with the heading \"(.*)\"/, async (expected) => {
-      const pageTitle = await driver.findElement(WebDriver.By.id('main-content'))
+      const pageTitle = await driver.findElement('#main-content');
       const actual = await pageTitle.getText()
       expect(actual).toContain(expected)
       driver.quit();
