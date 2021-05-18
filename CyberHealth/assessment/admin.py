cbe464eb5ce620c263a243cbe760431e937dbb14
kingdomlevel <nllmrrs@aol.com>
2021-05-18 15:19:27 +0100

    Register control in admin and allow horizontal filter for adding controls to pathway in admin

--- CyberHealth/assessment/admin.py
+++ CyberHealth/assessment/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from assessment.models import Organisation, OrganisationRegion, OrganisationType, Answer, Question, Choice, \
-    PathwayGroup, Pathway
+from assessment.models import *
 
 
 class PathwayGroupAdmin(admin.ModelAdmin):
@@ -10,11 +9,19 @@ class PathwayGroupAdmin(admin.ModelAdmin):
 class PathwayAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": (
         "short_name",)}
+    filter_horizontal = ('controls',)
+
+
+class ControlAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": (
+        "title",)}
 
 
 admin.site.register(Answer)
 admin.site.register(Question)
 admin.site.register(Choice)
+
+admin.site.register(Control, ControlAdmin)
 
 admin.site.register(PathwayGroup, PathwayGroupAdmin)
 admin.site.register(Pathway, PathwayAdmin)
