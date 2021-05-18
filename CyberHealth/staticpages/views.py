4f6ed4359a1dfd7a83a7f625f4c1b1000924e7c4
h0usemaintenance <h0usemaintenance@hotmail.com>
2021-05-18 12:55:31 +0100

    Merge the main branch

--- CyberHealth/staticpages/views.py
+++ CyberHealth/staticpages/views.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-@login_required
 def start_page(request):
     return render(request, 'staticpages/index.html')
 
