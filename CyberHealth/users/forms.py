04623405bfb94115ece1a5de6d0e4b2c60194df8
Adam Parsons <adam@adamparsons.me>
2021-05-19 16:12:18 +0100

    wip

--- CyberHealth/users/forms.py
+++ CyberHealth/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +13,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
+class MyAuthForm(AuthenticationForm):
+    email = forms.EmailField(max_length=255)
+  
+    error_messages = {
+        'invalid_login': _(
+            "Please enter a correct %(username)s and password. Note that both "
+            "fields may be case-sensitive."
+        ),
+        'inactive': _("This account is inactive."),
+    }
+    def clean(self):
+        cd = self.cleaned_data
+        if not cd.get('username'):
+            self.add_error('username', "username is blank")
+        return cd
+
+
