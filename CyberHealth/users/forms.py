ae2224643d37f10c310a93c7e9527f6c3eb8455d
Adam Parsons <adam@adamparsons.me>
2021-05-20 16:26:18 +0100

    added validation styling

--- CyberHealth/users/forms.py
+++ CyberHealth/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserRegisterForm(UserCreationForm):
@@ -13,4 +14,14 @@ class UserRegisterForm(UserCreationForm):
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
 
+class LoginForm(AuthenticationForm):
 
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)       
+        self.error_messages['invalid_login'] = "Enter a valid email \
+        and password combination.You will be locked out if you enter \
+        the wrong details 5 times."
+
+        for field in self.fields.values():
+            field.error_messages = {'required': 'Enter {fieldname}'.format(
+                fieldname=field.label).capitalize()}
--- CyberHealth/users/urls.py
+++ CyberHealth/users/urls.py
@@ -1,24 +1,34 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from users.forms import LoginForm
 
 
 urlpatterns = [
-    path('account_verification/<auth_token>/', views.account_activation, name='account-activation-page'),
+    path('account_verification/<auth_token>/', 
+         views.account_activation, name='account-activation-page'),
     path('error/', views.error_page, name="error-page"),
     path('verify_account/', views.send_token_page, name="send-token-page"),
     path('account_activated/', views.success_page, name="success-page"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+         template_name='users/logout.html'), name='logout'),
     path('password_reset/',
-         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+         auth_views.PasswordResetView.as_view(
+          template_name='users/password_reset.html'),
+         name='password_reset'),
     path('password_reset/done',
-         auth_views.PasswordResetDoneView.as_view(template_name='users/send_token.html'),
+         auth_views.PasswordResetDoneView.as_view(
+          template_name='users/send_token.html'),
          name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(
+          template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('password_reset_complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/success.html'),
+         auth_views.PasswordResetCompleteView.as_view(
+          template_name='users/success.html'),
          name='password_reset_complete'),
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(
+         template_name='users/login.html',
+         authentication_form=LoginForm), name='login'),
 ]
