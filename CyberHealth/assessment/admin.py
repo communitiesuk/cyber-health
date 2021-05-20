14c224dbc3b5528864f5889b2e788c4c782700f8
kingdomlevel <nllmrrs@aol.com>
2021-05-20 09:15:14 +0100

    removes commented out alternative approach to SubControl admin

--- CyberHealth/assessment/admin.py
+++ CyberHealth/assessment/admin.py
@@ -28,34 +28,9 @@ class QuestionInline(admin.TabularInline):
     fields = ('sort_order', 'question_text')
     extra = 0
 
-# class SubControlForm(forms.ModelForm):
-#     class Meta:
-#         model = SubControl
-#         fields = '__all__'
-
-#     questions = forms.ModelMultipleChoiceField(
-#                     queryset=Question.objects.filter(sub_control__isnull=True),
-#                     widget=forms.CheckboxSelectMultiple
-#                 )
-
-#     def __init__(self, *args, **kwargs):
-#         super(SubControlForm, self).__init__(*args, **kwargs)
-#         if self.instance:
-#             if self.instance.in_sub_control:
-#                 self.fields['questions'].initial = self.instance.in_sub_control.all()
-#             else:
-#                 self.fields['questions'].initial = []
-
-#     def save(self, *args, **kwargs):
-#         instance = super(SubControlForm, self).save(commit=False)
-#         self.fields['questions'].initial.update(sub_control=None)
-#         self.cleaned_data['questions'].update(sub_control=instance)
-#         return instance
-
 class SubControlAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": (
         "title",)}
-    # form = SubControlForm
     inlines = (QuestionInline,)
 
 
