71e1c14ef371998b04766966a566450438f8756c
Adam Parsons <adam@adamparsons.me>
2021-05-20 10:49:45 +0100

    removed custom validation

--- CyberHealth/users/forms.py
+++ CyberHealth/users/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import gettext_lazy as _
 
 
 class UserRegisterForm(UserCreationForm):
@@ -14,20 +12,5 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
-class MyAuthForm(AuthenticationForm):
-    email = forms.EmailField(max_length=255)
-  
-    error_messages = {
-        'invalid_login': _(
-            "Please enter a correct %(username)s and password. Note that both "
-            "fields may be case-sensitive."
-        ),
-        'inactive': _("This account is inactive."),
-    }
-    def clean(self):
-        cd = self.cleaned_data
-        if not cd.get('username'):
-            self.add_error('username', "username is blank")
-        return cd
 
 
