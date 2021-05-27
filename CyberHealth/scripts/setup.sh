c3c57dc3ba9866777059a8f4b11c35ffa2995328
kingdomlevel <nllmrrs@aol.com>
2021-05-27 12:23:24 +0100

    loading test data in one consistent way in the django project's setup.sh

--- CyberHealth/scripts/setup.sh
+++ CyberHealth/scripts/setup.sh
@@ -28,10 +28,7 @@ npm run frontend:build
 python3 manage.py migrate
 
 # load testdata
-python3 manage.py loaddata assessment/fixtures/testdata*.json
+python3 manage.py loaddata */fixtures/testdata*.json
 
 # copy all static files into STATIC_ROOT (static/assets)
 python3 manage.py collectstatic --noinput
-
-# load acceptance test data.
-python3 manage.py loaddata acceptance_test
