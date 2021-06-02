35995317414ccd2979217a587898b1e9899e858c
kingdomlevel <nllmrrs@aol.com>
2021-06-02 12:38:32 +0100

    allows for private storage

--- CyberHealth/CyberHealth/storage_backends.py
+++ CyberHealth/CyberHealth/storage_backends.py
@@ -3,5 +3,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class UploadStorage(S3Boto3Storage):
   location = 'uploads'
   file_overwrite = False
-  # default_acl = 'private'
-  # custom_domain = False
+  default_acl = 'private'
+  custom_domain = False
