0fcd25acef0021ba96d5b64f1ef0bb54f68da9f2
h0usemaintenance <h0usemaintenance@hotmail.com>
2021-06-04 13:52:11 +0100

    Triggering pipeline

--- CyberHealth/users/views.py
+++ CyberHealth/users/views.py
@@ -13,9 +13,9 @@ def send_user_notification(user_details, user_token, template_id='63d94931-3b5a-
     print(f'{settings.CLOUDFOUNDRY_SPACE} is the current environment value.')
 
     if settings.CLOUDFOUNDRY_SPACE in [settings.DEFAULT_CLOUDFOUNDRY_SPACE, settings.DEFAULT_CLOUDFOUNDRY_SPACE_UAT]:
-        account_verification_link = f'https://cyberhealth-{settings.CLOUDFOUNDRY_SPACE}.london.cloudapps.digital/account_verification/{user_token}'
+        account_verification_link = f'https://cyberhealth-{settings.CLOUDFOUNDRY_SPACE}.london.cloudapps.digital/account/account_verification/{user_token}'
     elif settings.CLOUDFOUNDRY_SPACE == settings.DEFAULT_CLOUDFOUNDRY_SPACE_FINAL:
-        account_verification_link = f'https://cyberhealth.london.cloudapps.digital/account_verification/{user_token}'
+        account_verification_link = f'https://cyberhealth.london.cloudapps.digital/account/account_verification/{user_token}'
     else:
         account_verification_link = f'http://{settings.CLOUDFOUNDRY_SPACE}:8000/account/account_verification/{user_token}'
     return settings.NOTIFICATIONS_CLIENT.send_email_notification(
