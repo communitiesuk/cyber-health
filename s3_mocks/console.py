b13bd4a15d44ff39d021d24b2c479a82d8cbd425
kingdomlevel <nllmrrs@aol.com>
2021-06-04 08:26:34 +0100

    additional functionality to poke around fake bucket in moto server

--- s3_mocks/console.py
+++ s3_mocks/console.py
@@ -1,6 +1,12 @@
+import pdb
 import boto3
 
 s3 = boto3.resource('s3', endpoint_url="http://localhost:5000")
+bucket = s3.Bucket('cyber-health-assets-test')
+bucket_contents = bucket.objects.all()
 
-for bucket in s3.Bucket('cyber-health-assets-test').objects.all():
+for bucket in bucket_contents:
     print(bucket.key)
+
+pdb.set_trace()
+pass
