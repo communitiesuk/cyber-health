c34efc07b5e773c9b3e66fb6b548f2b7d81d8ace
kingdomlevel <nllmrrs@aol.com>
2021-06-04 09:29:32 +0100

    catch no bucket in console

--- s3_mocks/console.py
+++ s3_mocks/console.py
@@ -2,11 +2,15 @@ import pdb
 import boto3
 
 s3 = boto3.resource('s3', endpoint_url="http://localhost:5000")
-bucket = s3.Bucket('cyber-health-assets-test')
-bucket_contents = bucket.objects.all()
+try:
+    bucket = s3.Bucket('cyber-health-assets-test')
+    bucket_contents = bucket.objects.all()
 
-for bucket in bucket_contents:
-    print(bucket.key)
+    for file in bucket_contents:
+        print(file.key)
 
-pdb.set_trace()
-pass
+    pdb.set_trace()
+    pass
+except s3.meta.client.exceptions.NoSuchBucket as err:
+    print(err)
+    print("Fake Bucket not found - try e.g. running tests")
