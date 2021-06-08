571fea8b662f5ff470694a02fd2237de48da98c3
kingdomlevel <nllmrrs@aol.com>
2021-06-08 13:09:03 +0100

    handle flip between (dev&&travis) and deploy w instance vars

--- CyberHealth/CyberHealth/storage_backends.py
+++ CyberHealth/CyberHealth/storage_backends.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class UploadStorage(S3Boto3Storage):
@@ -6,4 +7,4 @@ class UploadStorage(S3Boto3Storage):
   default_acl = 'private'
   # MAYBE: set this to true by default unless there is an env var to flag to use default AWS space
   #        and only set this env var to false in cloudfoundry
-  custom_domain = True    # defaults to True
+  custom_domain = settings.AWS_USE_CUSTOM_URL
