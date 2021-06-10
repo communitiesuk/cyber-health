8884a54112d102685a893a057eeb551ffa6be06d
Adam Parsons <adam@adamparsons.me>
2021-06-10 11:30:25 +0100

    change user list display in admin

--- CyberHealth/users/admin.py
+++ CyberHealth/users/admin.py
@@ -4,14 +4,44 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from .models import Organisation, OrganisationRegion, OrganisationType, OrganisationUser, UserProfile
 
+
+def admin_method_attributes(**outer_kwargs):
+    """ Wrap an admin method with passed arguments as attributes and values.
+    DRY way of extremely common admin manipulation such as setting short_description, allow_tags, etc.
+    """
+    def method_decorator(func):
+        for kw, arg in outer_kwargs.items():
+            setattr(func, kw, arg)
+        return func
+    return method_decorator
+
 class CustomUserAdmin(UserAdmin):
-     def get_form(self, request, obj, **kwargs):
+    list_display = ("_email", "_name", "is_staff")
+    readonly_fields = ('_email', )     
+
+    def _email(self, obj):
+        return obj.get_username()
+    _email.short_description = 'Email'
+    _email.admin_order_field = 'email'
+    
+    def _name(self, obj):
+        return obj.get_short_name()
+    _name.short_description = 'Name'
+    _name.admin_order_field = 'name'
+    
+    def get_form(self, request, obj, **kwargs):
         form = super(CustomUserAdmin, self).get_form(request, obj, **kwargs)
         form.base_fields['first_name'].label = 'Name'
         form.base_fields['username'].label = 'Email'
         form.base_fields["last_name"].widget = forms.HiddenInput()
         form.base_fields["email"].widget = forms.HiddenInput()
         return form
+
+            # fieldsets = (
+    #     ('Personal info', {'fields': ('first_name', 'email', 'password')}),
+    #     ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    #     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    # )
 
 admin.site.register(Organisation)
 admin.site.register(OrganisationRegion)
