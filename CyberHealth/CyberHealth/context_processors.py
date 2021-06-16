0838a4a80d39af38b0df72ed306402a4e89dc080
kingdomlevel <nllmrrs@aol.com>
2021-06-16 16:12:08 +0100

    store organisation details in session to limit DB fetches for analytics purposes

--- CyberHealth/CyberHealth/context_processors.py
+++ CyberHealth/CyberHealth/context_processors.py
@@ -2,26 +2,32 @@ from users.models import OrganisationRegion, OrganisationType
 
 def organisation_in_base_context(request):
     if (request.user.is_authenticated):
-        if len(request.user.organisation_set.values()) == 1:
-            # for users who are associated with one organisation
-            # ids
-            org_region_id = request.user.organisation_set.values()[0]['organisation_region_id']
-            org_type_id = request.user.organisation_set.values()[0]['organisation_type_id']
+        # user is logged in, get organisation details
+        # re-use from session if possible
+        session = request.session
+                
+        if session.get('organisation') is None:
+            if len(request.user.organisation_set.values()) == 1:
+                session['organisation'] = {}
+                # for users who are associated with one organisation
+                # ids
+                org_region_id = request.user.organisation_set.values()[0]['organisation_region_id']
+                org_type_id = request.user.organisation_set.values()[0]['organisation_type_id']
 
-            # strings
-            org_name = request.user.organisation_set.values()[0]['name']
-            org_type = OrganisationType.objects.get(pk=org_type_id).type
-            org_region = OrganisationRegion.objects.get(pk=org_region_id).name
+                # strings
+                session['organisation']['org_name'] = request.user.organisation_set.values()[0]['name']
+                session['organisation']['org_type'] = OrganisationType.objects.get(pk=org_type_id).type
+                session['organisation']['org_region'] = OrganisationRegion.objects.get(pk=org_region_id).name
+            else:
+                # once a user can be associated with multiple organisations, 
+                # work out which org user is logged in with
+                pass
 
-            return {
-                'organisation_name': org_name,
-                'organisation_type': org_type,
-                'organisation_region': org_region,
-            }
-        else:
-            # once a user can be associated with multiple organisations, 
-            # work out which org user is logged in with
-            pass
+        return {
+            'organisation_name': session['organisation']['org_name'],
+            'organisation_type': session['organisation']['org_type'],
+            'organisation_region': session['organisation']['org_region'],
+        }
 
 
     # no org details: return empty dict
