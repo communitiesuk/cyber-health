a3239568903f57b17f036159562b3af8338192c6
Adam Parsons <adam@adamparsons.me>
2021-06-18 10:59:20 +0100

    change export pdf view and template names

--- CyberHealth/staticpages/urls.py
+++ CyberHealth/staticpages/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('cookie-policy/', views.cookie_policy, name="cookie-policy"),
     path('accessibility-statement/', views.accessibility_statement, name="accessibility-statement"),
 	path('pdf-tests/', views.pdf_tests),
-    path('pdf-tests/privacy_view/', views.ViewPrivacyPDF.as_view(), name="pdf_view_privacy"),
-    path('pdf-tests/privacy_download/', views.DownloadPrivacyPDF.as_view(), name="pdf_download_privacy"),
-    path('pdf-tests/psn/', views.ViewPSNPDF.as_view(), name="pdf_view_psn"),
-    path('pdf-tests/new/', views.render_pdf_view, name="test-view"),
-    path('pdf-tests/weasy/', views.export_weasy_pdf, name="export_weasy"),
+    path('pdf-tests/psn/', views.render_to_pdf, name="export_psn_pdf"),
 ]
--- CyberHealth/staticpages/views.py
+++ CyberHealth/staticpages/views.py
@@ -1,25 +1,18 @@
-import os
 from django.shortcuts import render
-from io import BytesIO
-from xhtml2pdf import pisa
-from django.template.loader import get_template, render_to_string
+from django.template.loader import render_to_string
 from django.http import HttpResponse
-from django.views import View
-from assessment.models import Control
-from django.conf import settings
 from weasyprint import HTML
 import tempfile
-from django.db.models import Sum
 
 
-def export_weasy_pdf(request):
+def render_to_pdf(request):
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; attachment; filename="report.pdf"'
+    response['Content-Disposition'] = 'inline; attachment; filename="PSN_Code_of_Connection (CoCo).pdf"'
     response['Content-Transfer-Encoding'] = 'binary'
 
     html_string = render_to_string(
-        'weasy.html', {'expenses':[],'total': 0})
-    html = HTML(string=html_string)
+        'psn-coco.html', {})
+    html = HTML(string=html_string, base_url=request.build_absolute_uri())
 
     result = html.write_pdf()
 
@@ -31,66 +24,6 @@ def export_weasy_pdf(request):
         response.write(output.read())
 
     return response
-
-
-def render_pdf_view(request):
-    template_path = 'user_printer.html'
-    context = {'myvar': 'this is your template context'}
-    # Create a Django response object, and specify content_type as pdf
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-    # find the template and render it.
-    template = get_template(template_path)
-    html = template.render(context)
-
-    # create a pdf
-    pisa_status = pisa.CreatePDF(
-       html, dest=response)
-    # if error then show some funy view
-    if pisa_status.err:
-       return HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
-
-
-def render_to_pdf(template_src, context_dict={}):
-    template = get_template(template_src)
-    html  = template.render(context_dict)
-    result = BytesIO()
-    
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
-
-class ViewPrivacyPDF(View):
-	def get(self, request, *args, **kwargs):
-
-		pdf = render_to_pdf('staticpages/privacy-policy.html')
-		return HttpResponse(pdf, content_type='application/pdf')
-
-
-
-
-class DownloadPrivacyPDF(View):
-	def get(self, request, *args, **kwargs):
-		
-		pdf = render_to_pdf('staticpages/privacy-policy.html')
-
-		response = HttpResponse(pdf, content_type='application/pdf')
-		content = "attachment; filename=Privacy_test.pdf"
-		response['Content-Disposition'] = content
-		return response
-
-
-
-class ViewPSNPDF(View):
-    def get(self, request, *args, **kwargs):
-        controls = Control.objects.all()
-        # for control in controls:
-        #     print(control.subcontrol_set.all() )
-        pdf = render_to_pdf('pdf_template.html', {'controls': controls})
-        return HttpResponse(pdf, content_type='application/pdf')
-
 
 def start_page(request):
     return render(request, 'staticpages/index.html')
