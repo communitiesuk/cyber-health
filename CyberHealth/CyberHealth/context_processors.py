9bb11bba89cfbd1e7cc26a16cc8cdb899ea020e4
h0usemaintenance <h0usemaintenance@hotmail.com>
2021-06-25 11:10:42 +0100

    Correcting bug

--- CyberHealth/CyberHealth/context_processors.py
+++ CyberHealth/CyberHealth/context_processors.py
@@ -4,14 +4,13 @@ from users.models import OrganisationRegion, OrganisationType
 def organisation_in_base_context(request):
     """ Takes in a request and for users who are associated with one organisation adds the organisation information to
         that user's session if the user is authenticated. """
-
     if request.user.is_authenticated:
-        print(f'{request.user.is_authenticated} : user authenticated')
         session = request.session
-        print(f'{request.session.get("organisation")} : organisation details')
+
+        if request.user.is_superuser:
+            return {}
+
         if request.session.get('organisation') is None:
-            print(f'do you get here?')
-            print(f'{len(request.user.organisation_set.values())} : no of orgs')
             if len(request.user.organisation_set.values()) == 1:
                 session['organisation'] = {}
                 org_region_id = request.user.organisation_set.values()[0]['organisation_region_id']
@@ -21,9 +20,7 @@ def organisation_in_base_context(request):
                 session['organisation']['org_region'] = OrganisationRegion.objects.get(pk=org_region_id).name
             else:
                 # TODO: Add functionality for when a user is associated with multiple organisations
-                print(f'do you get here? 2')
                 pass
-
         return {
             'organisation_name': session['organisation']['org_name'],
             'organisation_type': session['organisation']['org_type'],
